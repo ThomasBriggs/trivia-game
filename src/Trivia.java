8df2c1796f9f00f4472d0cee5335120c1b137c4a
thomasbriggs <thomas.briggs333@gmail.com>
2019-11-30 14:47:49 +0000

    Cleaned up class

--- src/Trivia.java
+++ src/Trivia.java
@@ -9,18 +9,6 @@ import java.net.URL;
 public class Trivia {
 
     private static String baseUrl = "https://opentdb.com/api.php";
-    private static String defaultUrl = "https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple";
-
-    public static String get() throws Exception {
-        URL url = new URL(defaultUrl);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        InputStreamReader in = new InputStreamReader(conn.getInputStream());
-        BufferedReader br = new BufferedReader(in);
-        String output = br.readLine();
-        conn.disconnect();
-        return output; 
-    }
 
     public static String get(String... params) throws Exception {
         URL url = new URL(makeUrl(params));
