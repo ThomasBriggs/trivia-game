fbccf020e65c13517cfd48832b9897877f2b510b
thomasbriggs <thomas.briggs333@gmail.com>
2019-11-30 14:48:18 +0000

    Test Game

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,24 @@ import com.google.gson.Gson;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String json = Trivia.get("amount=3", "category=9", "difficulty=hard", "type=multiple");
+        String json = Trivia.get("amount=20", "category=9", "difficulty=easy", "type=multiple");
         Question[] questions = new Gson().fromJson(json, APIData.class).getQuestions();
         Scanner in = new Scanner(System.in);
+        int ans;
+        int questionNumber = 1;
         for (Question question : questions) {
-            System.out.println(question);
+            System.out.println(questionNumber + "\t" + question.toString(question.getAnswersSorted()));
             System.out.println();
-            
+            System.out.print("Please choose an answer (1-4): ");
+            ans = in.nextInt();
+            if (question.getAnswersSorted()[ans-1].equals(question.getCorrect_answer())){
+                System.out.println("Correct");
+            }else {
+                System.out.println("Incorrect");
+                System.out.println("Correct answer: " + question.getCorrect_answer());
+            }
+            System.out.println();
+            questionNumber++;
         }
     }
 }
