6d65a08914380a9cae0a14e53fbaff2fde9e3ffc
Thomas Briggs <thomas.briggs333@gmail.com>
2020-02-19 22:09:01 +0000

    Changed tests

--- src/CatagoryTest.java
+++ src/CatagoryTest.java
@@ -6,12 +6,13 @@ import Dao.*;
  */
 public class CatagoryTest {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String json = TriviaAPI.getCatagoryList();
         APIData data = new Gson().fromJson(json, APIData.class);
         Catagory[] cats = data.getCatagorys();
         for (Catagory catagory : cats) {
-            System.out.println(catagory.getName());
+            System.out.println(catagory.getName() + " - " + catagory.getId());
+
         }
     }
 }
