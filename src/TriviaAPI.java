af811ffc839f838f8c0081505cf62ee383822e41
Thomas Briggs <thomas.briggs333@gmail.com>
2020-02-19 22:10:44 +0000

    Changed variables to final and added Docs

--- src/TriviaAPI.java
+++ src/TriviaAPI.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -9,11 +10,31 @@ import java.net.URL;
  */
 public class TriviaAPI {
 
-    private static String baseUrl = "https://opentdb.com/api.php";
-    private static String catagoryUrl = "https://opentdb.com/api_category.php";
+    private final static String TRIVIADB_URL = "https://opentdb.com/api.php";
+    private final static String CATAGORY_URL = "https://opentdb.com/api_category.php";
+
+    /**
+     * Constructs a API URL from the base URL of the API and any paramaters for the
+     * API
+     * 
+     * @param baseUrl the base URL of the API
+     * @param params  any parameters to be given to the API
+     * @return a URL object made from the base URL and the Parameters
+     * @throws MalformedURLException
+     */
+    private static URL makeUrl(String baseUrl, String... params) throws MalformedURLException {
+        String url = baseUrl + "?";
+        for (int i = 0; i < params.length; i++) {
+            if (i > 0) {
+                url += "&";
+            }
+            url += params[i];
+        }
+        return new URL(url);
+    }
 
     public static String get(String... params) throws IOException {
-        URL url = new URL(makeUrl(baseUrl, params));
+        URL url = makeUrl(TRIVIADB_URL, params);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         InputStreamReader in = new InputStreamReader(conn.getInputStream());
@@ -23,20 +44,8 @@ public class TriviaAPI {
         return output;
     }
 
-    private static String makeUrl(String baseUrl, String... params) {
-        String output = baseUrl + "?";
-        for (int i = 0; i < params.length; i++) {
-            if (i > 0) {
-                output += "&";
-            }
-            output += params[i];
-        }
-        return output;
-    }
-
-    public static String getCatagoryList() throws Exception {
-
-        URL url = new URL(makeUrl(catagoryUrl));
+    public static String getCatagoryList() throws IOException {
+        URL url = makeUrl(CATAGORY_URL);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         InputStreamReader in = new InputStreamReader(conn.getInputStream());
