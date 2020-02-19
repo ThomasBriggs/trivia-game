6a86a0974dfa368be8e9dad3463570150dffd44a
Thomas Briggs <thomas.briggs333@gmail.com>
2020-02-19 21:04:16 +0000

    Removed unused methods

--- src/Dao/Question.java
+++ src/Dao/Question.java
@@ -1,6 +1,7 @@
 package Dao;
 
 import java.util.Arrays;
+
 import org.apache.commons.text.StringEscapeUtils;
 
 public class Question {
@@ -16,48 +17,24 @@ public class Question {
         return difficulty;
     }
 
-    public void setDifficulty(String difficulty) {
-        this.difficulty = difficulty;
-    }
-
     public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
+        return StringEscapeUtils.unescapeHtml4(this.question);
     }
 
     public String getCorrectAnswer() {
-        return StringEscapeUtils.unescapeHtml4(correct_answer);
-    }
-
-    public void setCorrectAnswer(String correct_answer) {
-        this.correct_answer = correct_answer;
+        return StringEscapeUtils.unescapeHtml4(this.correct_answer);
     }
 
     public String[] getIncorrectAnswers() {
         return incorrect_answers;
     }
 
-    public void setIncorrectAnswers(String[] incorrect_answers) {
-        this.incorrect_answers = incorrect_answers;
-    }
-
     public String getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
     public String getType() {
         return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public String[] getAnswers() {
@@ -75,17 +52,6 @@ public class Question {
         return answers;
     }
 
-    public String formatAnswers(String[] answers) {
-        String output;
-        String questionString = getQuestion();
-        String ans1 = answers[0];
-        String ans2 = answers[1];
-        String ans3 = answers[2];
-        String ans4 = answers[3];
-        output = String.format("%s\n1: %s\n2: %s\n3: %s\n4: %s", questionString, ans1, ans2, ans3, ans4);
-        return StringEscapeUtils.unescapeHtml4(output);
-    }
-
     /**
      * Returns the Topic Area of the question as well as the difficulty in the form
      * of Topic Area - Difficulty
@@ -93,8 +59,7 @@ public class Question {
      * @return string of the question title
      */
     public String getQuestionTitle() {
-        return this.getCategory() + " - " + this.getDifficulty().substring(0, 1).toUpperCase()
-                + this.getDifficulty().substring(1);
+        return getCategory() + " - " + getDifficulty().substring(0, 1).toUpperCase() + getDifficulty().substring(1);
     }
 
     @Override
