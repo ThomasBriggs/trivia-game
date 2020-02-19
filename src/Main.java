1d7e0360fc220ba2442b29ed4f879506a4841c57
Thomas Briggs <thomas.briggs333@gmail.com>
2020-02-19 22:09:18 +0000

    Changed main

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
-        TriviaGame game = new TriviaGame(10, 9, TriviaGame.QUESTION_DIFFICULTY_MEDIUM,
-                TriviaGame.QUESTION_TYPE_MULTIPLE);
+        TriviaGame game = new TriviaGame(10, 9, TriviaGame.QUESTION_DIFFICULTY_HARD, TriviaGame.QUESTION_TYPE_MULTIPLE);
         int answer;
         while (game.hasNextQuestion()) {
             System.out.println(game.getQuestionTitle());
-            System.out.println(game.getQuestion());
+            System.out.println(TriviaGame.escape(game.getQuestion().toString()));
             System.out.println(game.printAnswers());
             System.out.println();
             System.out.print("Please enter a (1-4): ");
